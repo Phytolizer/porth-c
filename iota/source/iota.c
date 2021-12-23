69bc05ad896f4b5e0e4a67812040255fa7b1c033
Kyle Coffey <mariorules24@gmail.com>
2021-12-22 23:13:47 -0600

    iota seems to work

--- iota/source/iota.c
+++ iota/source/iota.c
@@ -1,5 +1,7 @@
 #include "iota.h"
 
+#include <nonstd/strdup.h>
+#include <nonstd/strtok.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -24,31 +26,64 @@ bool IotaLoad(Iota* i, const char* filename) {
     return false;
   }
 
+  char* iota_prefix = NULL;
+  size_t iota = 0;
+
   char* line = NULL;
   size_t line_size = 0;
   while (getline(&line, &line_size, fp) != -1) {
-    if (line_size >= 1 && line[0] == '%') {
+    if (line_size == 0) {
+      free(line);
+      line = NULL;
+      continue;
+    }
+    *strrchr(line, '\n') = '\0';
+    if (line[0] == '%') {
       char* savep;
-      char* directive = strtok_r(line, " \t", &savep);
+      char* directive = nonstd_strtok_r(line, " ", &savep);
       if (directive == NULL) {
         fprintf(stderr, "Invalid directive: %s\n", line);
         free(line);
         return false;
       }
 
-      if (strcmp(directive, "prefix") == 0) {
-        char* prefix = strtok_r(NULL, " \t", &savep);
+      if (strcmp(&directive[1], "prefix") == 0) {
+        char* prefix = nonstd_strtok_r(NULL, " ", &savep);
         if (prefix == NULL) {
           fprintf(stderr, "%%prefix requires an argument\n");
           free(line);
           return false;
         }
+        iota_prefix = nonstd_strdup(prefix);
+      } else {
+        fprintf(stderr, "Unrecognized directive: %s\n", line);
+        free(line);
+        return false;
       }
+    } else {
+      char* name;
+      size_t name_len;
+      if (iota_prefix != NULL) {
+        name_len = strlen(iota_prefix) + strlen(line) + 1;
+        name = malloc(name_len);
+        strcpy(name, iota_prefix);
+        strcat(name, line);
+      } else {
+        name_len = strlen(line);
+        name = nonstd_strdup(line);
+      }
+      IotaEntryVec_push(
+          &i->entries, (IotaEntry){.name = nonstd_strdup(name), .value = iota});
+      IotaMap_insert(&i->map, (IotaMap_key){.key = name, .len = name_len},
+                     iota);
+      ++iota;
     }
     free(line);
     line = NULL;
   }
-  return false;
+
+  free(iota_prefix);
+  return true;
 }
 
 void IotaDeinit(Iota i) {
--- porth/source/main.c
+++ porth/source/main.c
@@ -1,3 +1,13 @@
+#include <config.h>
+#include <iota.h>
 #include <stdio.h>
 
-int main(int argc, char** argv) {}
+int main(int argc, char** argv) {
+  Iota i;
+  IotaInit(&i);
+  if (!IotaLoad(&i, PROJECT_SOURCE_DIR "/porth/iota/ops.iota")) {
+    return 1;
+  }
+
+  IotaDeinit(i);
+}
