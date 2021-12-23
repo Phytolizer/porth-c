60f932814b8cb97f2b5b4104e5e22972dcd6c1a7
Kyle Coffey <mariorules24@gmail.com>
2021-12-23 00:40:18 -0600

    remove debug output

--- porth/source/main.c
+++ porth/source/main.c
@@ -14,7 +14,6 @@ int main(int argc, char** argv) {
   if (!IotaLoad(&ops, PROJECT_SOURCE_DIR "/porth/iota/ops.iota")) {
     return 1;
   }
-  printf("Loaded %zu ops\n", ops.count);
 
   OpVec program;
   OpVec_init(&program);
