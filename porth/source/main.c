517535a08b028f6d23b7ff4a87d621ef17763c25
Kyle Coffey <mariorules24@gmail.com>
2021-12-24 02:24:19 -0600

    add subprocess.h (not working)

--- porth/source/main.c
+++ porth/source/main.c
@@ -6,6 +6,7 @@
 #include "porth/com.h"
 #include "porth/op.h"
 #include "porth/sim.h"
+#include "subprocess.h"
 
 static void PrintUsage(char* argv0);
 
@@ -38,6 +39,39 @@ int main(int argc, char** argv) {
     SimulateProgram(&ops, &program);
   } else if (strcmp(subcommand, "com") == 0) {
     CompileProgram(&ops, &program, "output.asm");
+    const char* nasm_command_line[] = {
+        "nasm",
+        "-f",
+        "elf64",
+        "output.asm",
+    };
+    const char* ld_command_line[] = {
+        "ld",
+        "-o",
+        "output",
+        "output.o",
+    };
+    struct subprocess_s process;
+    int ret = subprocess_create(
+        nasm_command_line,
+        subprocess_option_no_window | subprocess_option_combined_stdout_stderr,
+        &process);
+    if (ret != 0) {
+      fprintf(stderr, "ERROR: subprocess creation failed!\n");
+      return 1;
+    }
+    int return_code;
+    subprocess_join(&process, &return_code);
+    FILE* err = subprocess_stderr(&process);
+    char errbuf[1024];
+    int nread;
+    while ((nread = fread(errbuf, 1, sizeof(errbuf), err)) != 0) {
+      fwrite(errbuf, 1, nread, stderr);
+    }
+    if (return_code != 0) {
+      fprintf(stderr, "ERROR: subprocess exited with code %d\n", return_code);
+      return 1;
+    }
   } else {
     PrintUsage(argv[0]);
     fprintf(stderr, "ERROR: unknown subcommand %s\n", subcommand);
