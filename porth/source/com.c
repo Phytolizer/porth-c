cfde8085a6f43e633c2a5425919fae94e1b5941e
Kyle Coffey <mariorules24@gmail.com>
2021-12-24 06:49:58 -0600

    compilation

--- porth/source/com.c
+++ porth/source/com.c
@@ -2,9 +2,12 @@
 
 #include <assert.h>
 #include <errno.h>
-#include <main_asmt.h>
+#include <prefix_asmt.h>
 #include <stdio.h>
 #include <string.h>
+#include <suffix_asmt.h>
+
+#include "porth/op.h"
 
 void CompileProgram(Iota* ops, OpVec* program, const char* out_path) {
   FILE* fp = fopen(out_path, "w");
@@ -13,6 +16,27 @@ void CompileProgram(Iota* ops, OpVec* program, const char* out_path) {
             strerror(errno));
     return;
   }
-  fwrite(main_asmt, sizeof(char), main_asmt_size, fp);
+  fwrite(prefix_asmt, sizeof(char), prefix_asmt_size, fp);
+  for (size_t i = 0; i < program->length; i++) {
+    Op* op = program->data[i];
+    assert(ops->count == 4 && "Exhaustive handling of ops");
+    if (op->type == IotaValue(ops, "OP_PUSH")) {
+      OpPush* push_op = (OpPush*)op;
+      fprintf(fp, "    ;; -- push %ld --\n", push_op->value);
+      fprintf(fp, "    push %ld\n", push_op->value);
+    } else if (op->type == IotaValue(ops, "OP_PLUS")) {
+      fprintf(fp, "    ;; -- plus --\n");
+      fprintf(fp, "    ;; -- TODO: not implemented --\n");
+    } else if (op->type == IotaValue(ops, "OP_MINUS")) {
+      fprintf(fp, "    ;; -- minus --\n");
+      fprintf(fp, "    ;; -- TODO: not implemented --\n");
+    } else if (op->type == IotaValue(ops, "OP_DUMP")) {
+      fprintf(fp, "    ;; -- dump --\n");
+      fprintf(fp, "    ;; -- TODO: not implemented --\n");
+    } else {
+      assert(false && "unreachable");
+    }
+  }
+  fwrite(suffix_asmt, sizeof(char), suffix_asmt_size, fp);
   fclose(fp);
 }
